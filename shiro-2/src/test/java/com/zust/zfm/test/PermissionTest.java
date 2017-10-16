5bb0e74856946038382738a63e28588c9f2f9cbb
SprMeng <SuperMengZ@163.com>
2017-10-16 17:32:10 +0800

    授权

--- shiro-2/src/test/java/com/zust/zfm/test/PermissionTest.java
+++ shiro-2/src/test/java/com/zust/zfm/test/PermissionTest.java
@@ -4,19 +4,39 @@ import static org.junit.Assert.*;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 import org.apache.shiro.util.ThreadContext;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PermissionTest {
 
+	@Test/*(expected = UnauthorizedException.class)*/  
+	public void testCheckPermission () {  
+	    login("classpath:shiro-permission.ini", "zhang", "123");  
+	    //断言拥有权限：user:create  
+	    subject().checkPermission("user:create");  
+	    //断言拥有权限：user:delete and user:update  
+	    subject().checkPermissions("user:delete", "user:update");  
+	    //断言拥有权限：user:view 失败抛出异常  
+	    subject().checkPermissions("user:view");  
+	}   
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsPermitted() {
+		login("classpath:shiro-permission.ini", "zhang", "123");
+		
+		//判断拥有权限：user:create  
+	    Assert.assertTrue(subject().isPermitted("user:create"));  
+	    //判断拥有权限：user:update and user:delete  
+	    Assert.assertTrue(subject().isPermittedAll("user:update", "user:delete"));  
+	    //判断没有权限：user:view  
+	    Assert.assertFalse(subject().isPermitted("user:view"));  
 	}
 	
 	private Subject subject() {
